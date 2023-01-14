c76a6ed3977a7cb347ce0191e7355319c6098898
SRUSHTI-AGARWAL <51274343+SRUSHTI-AGARWAL@users.noreply.github.com>
2023-01-14 21:19:35 +0000

    Adding Content

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 4834738947;
+let accountEmail="Jane";
+var accountPassword ="12345";
+
+// var accountName = "John" // var s not used as it has issue of Block scope. 
+city= "Kashipur"; // variables in JS do not require the use of var or let. 
+
+let state;
+// accountId = 323232323; // assisgnement to const variable is not allowed. 
+
+console.log(accountId); 
+
+console.table([accountId, accountEmail,accountPassword,city,state])
---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,31 @@
+"use Srict";  // treat all code as newer versio of JS
+// all the code in file will be treated as newer standards of JS
+
+// alert("Hello"); // but this will not run here in node js ,but only in browser as JS Engine is inside browser.
+
+// Code must have readability
+
+// ecmascript international writes the standards for Javascript. 
+
+let number=3333;   
+let name= 'srishti'
+let isLoggedIn = true
+let price= 32.45
+
+
+// Data types
+
+/*  Primitive Data types: 
+number - 2 to power 53
+bigint
+string 
+boolean : true/ false
+null is a standalone value
+undefined: when a value is not defined and only initialized
+symbol is used when we make React Components, it is used to define UNIQUENESS of components 
+*/
+
+// OBJECT:  
+
+
+console.log(typeof(undefined))    // undefined is a type in JS
